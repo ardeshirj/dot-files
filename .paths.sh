747db1801c076f1e9628b1d9869444242a2bab25
ardeshirj <ardeshir.javaherchi@gmail.com>
2021-01-08 14:01:20 -0800

    Add GCP to .path.sh

--- .paths.sh
+++ .paths.sh
@@ -19,3 +19,10 @@ export PATH="$HOME/.cargo/bin:$PATH"
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
 # rvm - NOTE: rvm is expecting the line below to be the last one in the file!
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+# GCP
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/usr/local/google-cloud-sdk/path.zsh.inc' ]; then . '/usr/local/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/usr/local/google-cloud-sdk/completion.zsh.inc' ]; then . '/usr/local/google-cloud-sdk/completion.zsh.inc'; fi
